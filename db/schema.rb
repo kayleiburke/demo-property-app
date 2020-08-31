c88b09208071bc365ee5c55af254a169f64286de
Kaylei Burke <kaylei.burke@gmail.com>
2020-08-31 05:18:08 -0500

    Use postgres instead of sqlite.
    Heroku does not support sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_28_003024) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string "index"
     t.datetime "created_at", null: false
