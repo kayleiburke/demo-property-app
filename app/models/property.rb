9bd236249b129561594ca896b1d77c71813e4360
Kaylei Burke <kaylei.burke@gmail.com>
2020-09-01 11:03:33 -0500

    More code cleanup

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,8 @@ class Property < ApplicationRecord
   has_and_belongs_to_many :facilities
   has_and_belongs_to_many :property_types
   has_many_attached :property_images
+
+  def any_attached?
+    ActiveStorage::Attachment.where(record_type: model_name.to_s, record_id: id).any?
+  end
 end
